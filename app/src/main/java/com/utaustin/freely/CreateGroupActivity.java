3cd31dec92a1b93ae70a2f3db7adce9c010d3e87
Ashar Malik <ashar.malik.m@gmail.com>
2016-03-25 16:43:44 -0500

    Added next button to Create Group activity

--- app/src/main/java/com/utaustin/freely/CreateGroupActivity.java
+++ app/src/main/java/com/utaustin/freely/CreateGroupActivity.java
@@ -2,6 +2,9 @@ package com.utaustin.freely;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class CreateGroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.create_group_menu, menu);
+
+
+        return true;
     }
 }
