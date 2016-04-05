1308210121b1885b4b4c76877106b4d56974c3b0
Rohan Nagar <rohan.nagar@utexas.edu>
2016-04-04 22:48:57 -0500

    Add StatusActivity class

---
+++ app/src/main/java/com/utaustin/freely/activities/StatusActivity.java
@@ -0,0 +1,15 @@
+package com.utaustin.freely.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.utaustin.freely.R;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
