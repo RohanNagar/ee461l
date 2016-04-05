fba8ccf8ca390a20cf22d5450f709f9ebdb3bcc2
Ashar Malik <ashar.malik.m@gmail.com>
2016-04-04 22:38:13 -0500

    Added time picker

--- app/src/main/java/com/utaustin/freely/activities/CreateMeetingChooseTimeActivity.java
+++ app/src/main/java/com/utaustin/freely/activities/CreateMeetingChooseTimeActivity.java
@@ -1,18 +1,24 @@
 package com.utaustin.freely.activities;
 
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.utaustin.freely.R;
+import com.utaustin.freely.fragments.TimePickerFragment;
 
 public class CreateMeetingChooseTimeActivity extends AppCompatActivity {
 
     EditText nameText;
+    Button beginTimePickerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,17 @@ public class CreateMeetingChooseTimeActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         nameText = (EditText) findViewById(R.id.activity_create_meeting_choose_time_name);
+        beginTimePickerButton = (Button) findViewById(R.id.create_meeting_choose_activity_open_begin_picker);
+
+        final FragmentManager fragmentManager = getFragmentManager();
+
+        beginTimePickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(fragmentManager, "timePicker");
+            }
+        });
     }
 
     @Override
